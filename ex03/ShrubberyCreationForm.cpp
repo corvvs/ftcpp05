973d6e8aeb315f266549a1f0ff1ef8ff80f4b779
河田鷹介 <benefium@gmail.com>
2021-11-17 14:01:47 +0900

    forest

--- ex03/ShrubberyCreationForm.cpp
+++ ex03/ShrubberyCreationForm.cpp
@@ -28,44 +28,44 @@ void    ShrubberyCreationForm::executeCore(void) const {
         ofs.exceptions(std::ios::failbit | std::ios::badbit);
         ofs.open(filename);
         ofs
-            << "                                    ..." << std::endl
-            << "                                  .+MMMMMNL" << std::endl
-            << "                                 ,MMMMMMMMF" << std::endl
-            << "                                     MMMMM    ..." << std::endl
-            << "                                     dMMMMgMMMMMMMMNa," << std::endl
-            << "                                  ..JMMMMMMMMMMMMMMMM" << std::endl
-            << "                             ..JNMMMMMMMMMMM#9\"!" << std::endl
-            << "                       ...gMMMMMMMMMMMMMM\"`" << std::endl
-            << "                ....gNMMMMMMMMMMMMW\"WMMM]" << std::endl
-            << "                 ?TMMMMMMMMHWM#=    JMMMb .....-JJJ....." << std::endl
-            << "                          .gM@    ..dMMMMMMMMMMMMMMMMMMMMNe" << std::endl
-            << "                        .MMMD..JNMMB4MMM#`       ?TMMMMMMM@`" << std::endl
-            << "                      .MMMMMMH\"^    .MMM#       ..dMB\"\"^" << std::endl
-            << "                    .MMMMM\"!        .MMMN_     ?`" << std::endl
-            << "                  .MMM#\"      TNg++MMMMMM:     .gMMNJ." << std::endl
-            << "                .MMM\"`         (MMMMMMMM#     .MMMMMMMN-" << std::endl
-            << "                .#^   .+gJ,      ?MMMMMMt         ?MMM#`" << std::endl
-            << "                      TMMMM:       7MMMD          .MMMN,....J-.." << std::endl
-            << "                        MMN,..                  ..gMMMMMMMMMMMMMMp" << std::endl
-            << "                       .MMMMMMMMMm           .+MMMMMMMMMMB\"\"\"\"\"\"\"^" << std::endl
-            << "                   ..+MMMMMMMMMMB=       ..MMMMMMMMMMM]" << std::endl
-            << "               ..+MMMMMMMMMM#\"`       .JMMMMM9^(MMMMMM{" << std::endl
-            << "            .JMMMMMMMMMMMM`        .gMMMM9!  .dMMMMMMM`" << std::endl
-            << "        ..MMMMMMMM#\"4MMMMM      .gMMM#=     .MMMM=MMMM" << std::endl
-            << "    ..gMMMMMMB\"!   .MMMMMM_  .dMMMB^      .MMMMY  MMM#" << std::endl
-            << "    ?""""!        .MMMMMMMmgMMMM=       .dMMMB` ..MMMN........." << std::endl
-            << "                 .MMMMMMMMMMM9!       .&MMMMNgMMMMMMMMMMMMMMMMMMMNN&.." << std::endl
-            << "                (MMMMMMMMMM\"        .+MMMMMMB\"7`  dMMM] _7TMMMMMMMMMMMNg," << std::endl
-            << "              .MMMMMMMMMMF        .uMMMM#\"`       JMMM]      -TMMMMMMMMMMNe." << std::endl
-            << "            .jMMMMMMMMMMM]   .a .dMMMM#`          JMMM]          +MMMMMMMMMb" << std::endl
-            << "           .MMMMMMMM\"`dMM]  (MNMMMM#! MN.         JMMM]        .+MMMMMMMMMM5" << std::endl
-            << "         .MMMMMMM#^   MMMt .MMMMM#^    WN,        JMMM]        !~``" << std::endl
-            << "       .+MMMMMM#'    .MMM) MMMM\"`       ?MN,      JMMM]" << std::endl
-            << "      .MMMMMM#!      dMMM]  `            .MMMa....MMMMF" << std::endl
-            << "      dMMMMB!        JMMMF                 TMMMMMMMMMMF" << std::endl
-            << "      dMM@!          .MMM\\                  ?MMMMMMMMF" << std::endl
-            << "       ?               7\"                    /MMMMMMF" << std::endl
-            << "                                               TMMMt" << std::endl;
+            << "                                    ...                                      " << std::endl
+            << "                                  .+MMMMMNL                                  " << std::endl
+            << "                                 ,MMMMMMMMF                                  " << std::endl
+            << "                                     MMMMM    ...                            " << std::endl
+            << "                                     dMMMMgMMMMMMMMNa,                       " << std::endl
+            << "                                  ..JMMMMMMMMMMMMMMMM                        " << std::endl
+            << "                             ..JNMMMMMMMMMMM#9'!                             " << std::endl
+            << "                       ...gMMMMMMMMMMMMMM'`                                  " << std::endl
+            << "                ....gNMMMMMMMMMMMMW'WMMM]                                    " << std::endl
+            << "                 ?TMMMMMMMMHWM#=    JMMMb .....-JJJ.....                     " << std::endl
+            << "                          .gM@    ..dMMMMMMMMMMMMMMMMMMMMNe                  " << std::endl
+            << "                        .MMMD..JNMMB4MMM#`       ?TMMMMMMM@`                 " << std::endl
+            << "                      .MMMMMMH'^    .MMM#       ..dMB''^                     " << std::endl
+            << "                    .MMMMM'!        .MMMN_     ?`                            " << std::endl
+            << "                  .MMM#'      TNg++MMMMMM:     .gMMNJ.                       " << std::endl
+            << "                .MMM'`         (MMMMMMMM#     .MMMMMMMN-                     " << std::endl
+            << "                .#^   .+gJ,      ?MMMMMMt         ?MMM#`                     " << std::endl
+            << "                      TMMMM:       7MMMD          .MMMN,....J-..             " << std::endl
+            << "                        MMN,..                  ..gMMMMMMMMMMMMMMp           " << std::endl
+            << "                       .MMMMMMMMMm           .+MMMMMMMMMMB'''''''^           " << std::endl
+            << "                   ..+MMMMMMMMMMB=       ..MMMMMMMMMMM]                      " << std::endl
+            << "               ..+MMMMMMMMMM#'`       .JMMMMM9^(MMMMMM{                      " << std::endl
+            << "            .JMMMMMMMMMMMM`        .gMMMM9!  .dMMMMMMM`                      " << std::endl
+            << "        ..MMMMMMMM#'4MMMMM      .gMMM#=     .MMMM=MMMM                       " << std::endl
+            << "    ..gMMMMMMB'!   .MMMMMM_  .dMMMB^      .MMMMY  MMM#                       " << std::endl
+            << "    ?""""!        .MMMMMMMmgMMMM=       .dMMMB` ..MMMN.........              " << std::endl
+            << "                 .MMMMMMMMMMM9!       .&MMMMNgMMMMMMMMMMMMMMMMMMMNN&..       " << std::endl
+            << "                (MMMMMMMMMM'        .+MMMMMMB'7`  dMMM] _7TMMMMMMMMMMMNg,    " << std::endl
+            << "              .MMMMMMMMMMF        .uMMMM#'`       JMMM]      -TMMMMMMMMMMNe. " << std::endl
+            << "            .jMMMMMMMMMMM]   .a .dMMMM#`          JMMM]          +MMMMMMMMMb " << std::endl
+            << "           .MMMMMMMM'`dMM]  (MNMMMM#! MN.         JMMM]        .+MMMMMMMMMM5 " << std::endl
+            << "         .MMMMMMM#^   MMMt .MMMMM#^    WN,        JMMM]        !~``          " << std::endl
+            << "       .+MMMMMM#'    .MMM) MMMM'`       ?MN,      JMMM]                      " << std::endl
+            << "      .MMMMMM#!      dMMM]  `            .MMMa....MMMMF                      " << std::endl
+            << "      dMMMMB!        JMMMF                 TMMMMMMMMMMF                      " << std::endl
+            << "      dMM@!          .MMM\\                  ?MMMMMMMMF                      " << std::endl
+            << "       ?               7'                    /MMMMMMF                        " << std::endl
+            << "                                               TMMMt                         " << std::endl;
     }
     std::cout
         << Constants::kTextInfo
